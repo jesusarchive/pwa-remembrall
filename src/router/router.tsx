d6c6371ee3a494d26505575ad812c2c385837839
jesusarchive <jbrdev21880949243098@protonmail.com>
2024-11-02 19:58:32 +0100

    update tsconfig paths

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import ErrorBoundary from "../components/error-boundary/error-boundary";
-import GamePage from "../pages/game-page";
-import HomePage from "../pages/home-page";
+import ErrorBoundary from "@/components/error-boundary";
+import GamePage from "@/pages/game-page";
+import HomePage from "@/pages/home-page";
 
 export const routes = [
   {
