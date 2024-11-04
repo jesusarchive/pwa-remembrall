9232526b47d340b22bc054829608ecbe9436327e
jesusarchive <jbrdev21880949243098@protonmail.com>
2024-11-04 10:10:56 +0100

    update components

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,5 +1,15 @@
+import { cn } from "@/utils/cn";
+
 export type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
 
-export default function Button(props: Readonly<ButtonProps>) {
-  return <button {...props} />;
+export default function Button({ className, ...props }: Readonly<ButtonProps>) {
+  return (
+    <button
+      className={cn(
+        "bg-blue-900 text-white font-bold py-2 px-4 rounded",
+        className
+      )}
+      {...props}
+    />
+  );
 }
--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,5 +1,12 @@
+import { cn } from "@/utils/cn";
+
 export type InputProps = React.HTMLAttributes<HTMLInputElement>;
 
-export default function Input(props: Readonly<InputProps>) {
-  return <input {...props} />;
+export default function Input({ className, ...props }: Readonly<InputProps>) {
+  return (
+    <input
+      className={cn("border-gray-300 border-2 rounded p-2 mb-4", className)}
+      {...props}
+    />
+  );
 }
