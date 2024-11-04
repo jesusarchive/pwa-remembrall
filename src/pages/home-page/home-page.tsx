a11f0e937bda358c8527240510171d3dfe2d12d3
jesusarchive <jbrdev21880949243098@protonmail.com>
2024-11-04 10:11:11 +0100

    home page wip

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,3 +1,14 @@
+import Button from "@/components/ui/button";
+import Input from "@/components/ui/input";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="h-dvh w-dvw flex justify-center">
+      <div className="flex flex-col gap-4 pt-20">
+        <span className="self-center">🖱️</span>
+        <Input className="w-96" placeholder="Name" />
+        <Button>Start</Button>
+      </div>
+    </div>
+  );
 }
