f083aedad35dfdfa02d423229c9ddf4827c6f7f1
jesusarchive <jbrdev21880949243098@protonmail.com>
2024-11-05 11:05:53 +0100

    add vite pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import "./index.css";
 
 import { createRoot } from "react-dom/client";
+import { registerSW } from "virtual:pwa-register";
 
 import App from "./app.tsx";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      // cache all the imports
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      // cache all the static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        scope: "/",
+        start_url: "/",
+      },
+    }),
+  ],
+});
